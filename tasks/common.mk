4420540eb50208a016f051ecc09ca5348254754a
Alexander Zinovyev <alexions@Alexanders-MacBook-Air.local>
2016-03-25 01:03:51 +0300

    Makefile: target to run php added.

--- tasks/common.mk
+++ tasks/common.mk
@@ -1,6 +1,25 @@
+.PHONY: all run
+
 all:
-	@echo "'make input' - Create input.txt file."
+	@echo "'make input' - Create the input.txt file."
+	@echo "'make run ENV' - Run specified env"
+	@echo "    ENV = erlang | golang | nodejs | php | python"
+	@echo "'make run all' - Run all envs"
 
 input:
-	@echo "Paste your text and press 'Enter' and 'Ctrl + C' then to save the file"
+	@echo "Paste your text and press 'Enter' and 'Ctrl + D' then to save the file"
 	@cat > input.txt
+
+run: run-$(word 2, $(MAKECMDGOALS))
+
+run-php:
+	@if [ ! -f input.txt ]; then\
+		$(MAKE) input;\
+	fi
+	php php/index.php $(shell pwd)/input.txt
+
+run-erlang:
+	@echo "Run Erlang"
+
+%:
+	@:
