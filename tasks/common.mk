866f90df6643841ab8091dafdd2aade450067b20
Alexander Zinovyev <alexions@Alexanders-MacBook-Air.local>
2016-03-26 00:34:54 +0300

    Ability to run erlang tasks added.

--- tasks/common.mk
+++ tasks/common.mk
@@ -1,25 +1,30 @@
 .PHONY: all run
 
+INPUT_FILE = $(shell pwd)/input.txt
+
 all:
 	@echo "'make input' - Create the input.txt file."
 	@echo "'make run ENV' - Run specified env"
-	@echo "    ENV = erlang | golang | nodejs | php | python"
+	@echo "    ENV = erl | go | node | php | python"
 	@echo "'make run all' - Run all envs"
+
+check-input:
+	@if [ ! -f input.txt ]; then\
+		$(MAKE) input;\
+	fi
 
 input:
 	@echo "Paste your text and press 'Enter' and 'Ctrl + D' then to save the file"
 	@cat > input.txt
 
-run: run-$(word 2, $(MAKECMDGOALS))
+run: check-input run-$(word 2, $(MAKECMDGOALS))
 
 run-php:
-	@if [ ! -f input.txt ]; then\
-		$(MAKE) input;\
-	fi
-	php php/index.php $(shell pwd)/input.txt
+	php php/index.php $(INPUT_FILE)
 
-run-erlang:
-	@echo "Run Erlang"
+run-erl:
+	erlc -o erlang erlang/*.erl
+	erl -pa erlang -noshell -s main start $(INPUT_FILE) -s init stop
 
 %:
 	@:
