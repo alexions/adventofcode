19f14d698e0a8a10655c1ac014f9f51c58b49903
Alexander Zinoviev <azinoviev@machinezone.com>
2016-04-28 18:14:44 -0700

    Day3: Erlang solution: Second part solved.

--- tasks/day-3/erlang/main.erl
+++ tasks/day-3/erlang/main.erl
@@ -3,16 +3,29 @@
 
 start([InputFile]) ->
     {ok, Data} = file:read_file(InputFile),
-    {Set, _} = lists:foldl(fun(Direction, {Set, Position}) ->
-            NewPosition = case Direction of
-                $^ -> {element(1, Position), element(2, Position) + 1};
-                $v -> {element(1, Position), element(2, Position) - 1};
-                $> -> {element(1, Position) + 1, element(2, Position)};
-                $< -> {element(1, Position) - 1, element(2, Position)};
-                _ -> Position
-            end,
-            {sets:add_element(NewPosition, Set), NewPosition}
-        end, {sets:from_list([{0, 0}]), {0, 0}}, binary_to_list(Data)),
+    {SantaSet, _} = lists:foldl(fun move/2, {sets:from_list([{0, 0}]), {0, 0}}, binary_to_list(Data)),
 
-    io:format("Houses with presents: ~w~n", [sets:size(Set)]),
+    {SantaWRobotSet, _, _, _} = lists:foldl(fun(Direction, {Set, Santa, Robot, Switcher}) ->
+            case Switcher of
+                true ->
+                    {NewSet, NewPosition} = move(Direction, {Set, Santa}),
+                    {NewSet, NewPosition, Robot, not Switcher};
+                false ->
+                    {NewSet, NewPosition} = move(Direction, {Set, Robot}),
+                    {NewSet, Santa, NewPosition, not Switcher}
+            end
+        end, {sets:from_list([{0, 0}]), {0, 0}, {0, 0}, true}, binary_to_list(Data)),
+
+    io:format("Houses with presents (only Santa): ~w~n", [sets:size(SantaSet)]),
+    io:format("Houses with presents (Santa with robot): ~w~n", [sets:size(SantaWRobotSet)]),
     halt().
+
+move(Direction, {Set, {X, Y}}) ->
+    NewPosition = case Direction of
+        $^ -> {X, Y + 1};
+        $v -> {X, Y - 1};
+        $> -> {X + 1, Y};
+        $< -> {X - 1, Y};
+        _ -> {X, Y}
+    end,
+    {sets:add_element(NewPosition, Set), NewPosition}.
