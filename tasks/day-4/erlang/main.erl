c36f31fba2503c3e2d727494711902dca8b2142f
Alexander Zinoviev <azinoviev@machinezone.com>
2016-07-06 12:15:06 +0400

    Day4: Erlang solution added.

--- tasks/day-4/erlang/main.erl
+++ tasks/day-4/erlang/main.erl
@@ -3,12 +3,25 @@
 
 start([InputFile]) ->
     {ok, Puzzle} = file:read_file(InputFile),
-    io:format("5-0 index: ~s~n", [erlang:md5("hello")]),
+    io:format("5-0 index: ~w~n", [search_index(binary:bin_to_list(Puzzle), 0, fun(Hash) ->
+            case Hash of
+                <<0, 0, H, _/binary>> when H >= 16#0, H =< 16#F -> true;
+                _ -> false
+            end
+        end
+        )]),
+    io:format("6-0 index: ~w~n", [search_index(binary:bin_to_list(Puzzle), 0, fun(Hash) ->
+            case Hash of
+                <<0, 0, 0, _/binary>> -> true;
+                _ -> false
+            end
+        end
+        )]),
     halt().
 
-md5_check(Puzzle, Index) ->
+search_index(Puzzle, Index, Check) ->
     Hash = erlang:md5(Puzzle ++ integer_to_list(Index)),
-    case Hash of
-        <<00000, _>> -> Index;
-        _ -> md5_check(Puzzle, Index + 1)
+    case Check(Hash) of
+        true -> Index;
+        _ -> search_index(Puzzle, Index + 1, Check)
     end.
